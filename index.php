16a6dc6f2f195b2d8c3800cd47638bc7457f0c09
adventin <m1@seo-me.ru>
2020-06-18 18:15:30 +0300

    test4

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+
+class A {
+
+    public function f($a){
+
+        if (!is_string($a) || !method_exists($this, $a)) {
+            echo 'method not exists';
+            return;
+        }
+        $this->$a();
+    }
+
+    public function example(){
+        echo 'do something';
+    }
+}
+
+$a = new A();
+$a->f(123);
+$a->f('example1');
+$a->f('example');
